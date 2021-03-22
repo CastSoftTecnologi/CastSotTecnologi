6d937318fc6d5abb86c55ba61655aeb3befc7a63
hiperdark <hiperdark@hotmail.com>
2021-03-22 11:25:58 -0300

    Carlos Castillo: Ajustes de diseño y muestreo de los datos registrados

--- SisFact/SisFact/frmProductos.Designer.cs
+++ SisFact/SisFact/frmProductos.Designer.cs
@@ -30,11 +30,17 @@ namespace SisFact
         private void InitializeComponent()
         {
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.LProductos = new System.Windows.Forms.DataGridView();
+            this.lbbusqueda = new System.Windows.Forms.Label();
+            this.txtBusqueda = new System.Windows.Forms.TextBox();
             this.btnImprimir = new System.Windows.Forms.Button();
             this.btnModificar = new System.Windows.Forms.Button();
             this.btnAgregar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LProductos = new System.Windows.Forms.DataGridView();
+            this.cProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.x_producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.i_precioUnitario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m_activo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.LProductos)).BeginInit();
             this.SuspendLayout();
@@ -43,6 +49,8 @@ namespace SisFact
             // 
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.lbbusqueda);
+            this.panel2.Controls.Add(this.txtBusqueda);
             this.panel2.Controls.Add(this.btnImprimir);
             this.panel2.Controls.Add(this.btnModificar);
             this.panel2.Controls.Add(this.btnAgregar);
@@ -51,6 +59,55 @@ namespace SisFact
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(784, 174);
             this.panel2.TabIndex = 4;
+            // 
+            // lbbusqueda
+            // 
+            this.lbbusqueda.AutoSize = true;
+            this.lbbusqueda.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbbusqueda.ForeColor = System.Drawing.Color.White;
+            this.lbbusqueda.Location = new System.Drawing.Point(14, 108);
+            this.lbbusqueda.Name = "lbbusqueda";
+            this.lbbusqueda.Size = new System.Drawing.Size(49, 17);
+            this.lbbusqueda.TabIndex = 7;
+            this.lbbusqueda.Text = "Codigo";
+            // 
+            // txtBusqueda
+            // 
+            this.txtBusqueda.Location = new System.Drawing.Point(108, 105);
+            this.txtBusqueda.Name = "txtBusqueda";
+            this.txtBusqueda.Size = new System.Drawing.Size(327, 20);
+            this.txtBusqueda.TabIndex = 6;
+            this.txtBusqueda.TextChanged += new System.EventHandler(this.txtBusqueda_TextChanged);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImprimir.Location = new System.Drawing.Point(677, 148);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(104, 23);
+            this.btnImprimir.TabIndex = 5;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnModificar.Location = new System.Drawing.Point(677, 119);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(104, 23);
+            this.btnModificar.TabIndex = 4;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAgregar.Location = new System.Drawing.Point(677, 90);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(104, 23);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -65,41 +122,52 @@ namespace SisFact
             // 
             // LProductos
             // 
+            this.LProductos.AllowUserToAddRows = false;
+            this.LProductos.AllowUserToDeleteRows = false;
             this.LProductos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.LProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.LProductos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cProducto,
+            this.x_producto,
+            this.i_precioUnitario,
+            this.m_activo});
             this.LProductos.Location = new System.Drawing.Point(12, 192);
             this.LProductos.Name = "LProductos";
-            this.LProductos.Size = new System.Drawing.Size(776, 337);
+            this.LProductos.ReadOnly = true;
+            this.LProductos.Size = new System.Drawing.Size(784, 337);
             this.LProductos.TabIndex = 7;
+            this.LProductos.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.LProductos_ColumnHeaderMouseClick);
             // 
-            // btnImprimir
+            // cProducto
             // 
-            this.btnImprimir.Location = new System.Drawing.Point(340, 105);
-            this.btnImprimir.Name = "btnImprimir";
-            this.btnImprimir.Size = new System.Drawing.Size(104, 23);
-            this.btnImprimir.TabIndex = 5;
-            this.btnImprimir.Text = "Imprimir";
-            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.cProducto.DataPropertyName = "cProducto";
+            this.cProducto.HeaderText = "Código";
+            this.cProducto.Name = "cProducto";
+            this.cProducto.ReadOnly = true;
             // 
-            // btnModificar
+            // x_producto
             // 
-            this.btnModificar.Location = new System.Drawing.Point(340, 76);
-            this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(104, 23);
-            this.btnModificar.TabIndex = 4;
-            this.btnModificar.Text = "Modificar";
-            this.btnModificar.UseVisualStyleBackColor = true;
+            this.x_producto.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.x_producto.DataPropertyName = "x_producto";
+            this.x_producto.HeaderText = "Descripción";
+            this.x_producto.Name = "x_producto";
+            this.x_producto.ReadOnly = true;
             // 
-            // btnAgregar
+            // i_precioUnitario
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(340, 47);
-            this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(104, 23);
-            this.btnAgregar.TabIndex = 3;
-            this.btnAgregar.Text = "Agregar";
-            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.i_precioUnitario.DataPropertyName = "i_precioUnitario";
+            this.i_precioUnitario.HeaderText = "Precio.U";
+            this.i_precioUnitario.Name = "i_precioUnitario";
+            this.i_precioUnitario.ReadOnly = true;
+            // 
+            // m_activo
+            // 
+            this.m_activo.DataPropertyName = "m_activo";
+            this.m_activo.HeaderText = "Activo";
+            this.m_activo.Name = "m_activo";
+            this.m_activo.ReadOnly = true;
             // 
             // frmProductos
             // 
@@ -127,5 +195,11 @@ namespace SisFact
         private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.DataGridView LProductos;
+        private System.Windows.Forms.Label lbbusqueda;
+        private System.Windows.Forms.TextBox txtBusqueda;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn x_producto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn i_precioUnitario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m_activo;
     }
 }
--- SisFact/SisFact/frmProductos.cs
+++ SisFact/SisFact/frmProductos.cs
@@ -13,6 +13,7 @@ namespace SisFact
     public partial class frmProductos : Form
     {
         Acceso A = new Acceso();
+        string Campo;
         public frmProductos()
         {
             InitializeComponent();
@@ -20,9 +21,40 @@ namespace SisFact
 
         private void frmProductos_Load(object sender, EventArgs e)
         {
-
+            Diseno_grilla();
+            buscar_Registros();
+           
         }
 
-      
+        private void Diseno_grilla() {
+            // esto pasa también cuando toco la cabezera de la columna  --- solo aqui queda por difault la primera columna
+            lbbusqueda.Text = LProductos.Columns[0].HeaderText;//aqui indico al usuario por cual campo estoy filtrando
+            Campo = LProductos.Columns[0].Name.ToString();//aqui indico a la variable campo por cual campo debe filtrar
+            LProductos.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        }
+        private void buscar_Registros() {
+
+            string Sql = "select top 100 cProducto,x_producto,i_precioUnitario,m_activo from TPRODUCTO";
+            if (txtBusqueda.Text != "") { 
+                Sql = Sql + " where " + Campo  + " like '%" + txtBusqueda.Text  + "%'";
+            }
+
+            A.Consulta(Sql, "R");
+            LProductos.DataSource = A.ds.Tables["R"];
+            A.conexion.Close();
+        }
+
+        private void txtBusqueda_TextChanged(object sender, EventArgs e)
+        {
+            buscar_Registros();
+        }
+        private void LProductos_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //esto es para cambiar el filtro por cabezera de la Grilla
+            lbbusqueda.Text = LProductos.Columns[e.ColumnIndex].HeaderText;
+            Campo = LProductos.Columns[e.ColumnIndex].Name.ToString();
+        }
+
+
     }
 }
