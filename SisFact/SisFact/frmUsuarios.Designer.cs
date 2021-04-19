b2e19455a226a2bbf6f0b79c8ae4effeaac8b015
hiperdark <hiperdark@hotmail.com>
2021-04-19 15:15:00 -0300

    Carlos Castillo creo un formulario de Usuario

---
+++ SisFact/SisFact/frmUsuarios.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace SisFact
+{
+    partial class frmUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.LProductos = new System.Windows.Forms.DataGridView();
+            this.cProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.x_producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.i_precioUnitario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m_activo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.LProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LProductos
+            // 
+            this.LProductos.AllowUserToAddRows = false;
+            this.LProductos.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.LProductos.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.LProductos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LProductos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.LProductos.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.LProductos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.LProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.LProductos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cProducto,
+            this.x_producto,
+            this.i_precioUnitario,
+            this.m_activo});
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.LProductos.DefaultCellStyle = dataGridViewCellStyle6;
+            this.LProductos.EnableHeadersVisualStyles = false;
+            this.LProductos.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.LProductos.Location = new System.Drawing.Point(12, 152);
+            this.LProductos.MultiSelect = false;
+            this.LProductos.Name = "LProductos";
+            this.LProductos.ReadOnly = true;
+            this.LProductos.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.LProductos.RowHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.LProductos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.LProductos.Size = new System.Drawing.Size(939, 337);
+            this.LProductos.TabIndex = 8;
+            // 
+            // cProducto
+            // 
+            this.cProducto.DataPropertyName = "cProducto";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            this.cProducto.DefaultCellStyle = dataGridViewCellStyle3;
+            this.cProducto.HeaderText = "Código";
+            this.cProducto.Name = "cProducto";
+            this.cProducto.ReadOnly = true;
+            this.cProducto.Width = 80;
+            // 
+            // x_producto
+            // 
+            this.x_producto.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.x_producto.DataPropertyName = "x_producto";
+            this.x_producto.HeaderText = "Descripción";
+            this.x_producto.Name = "x_producto";
+            this.x_producto.ReadOnly = true;
+            // 
+            // i_precioUnitario
+            // 
+            this.i_precioUnitario.DataPropertyName = "i_precioUnitario";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopRight;
+            dataGridViewCellStyle4.Format = "N2";
+            dataGridViewCellStyle4.NullValue = null;
+            this.i_precioUnitario.DefaultCellStyle = dataGridViewCellStyle4;
+            this.i_precioUnitario.HeaderText = "Precio U.";
+            this.i_precioUnitario.Name = "i_precioUnitario";
+            this.i_precioUnitario.ReadOnly = true;
+            // 
+            // m_activo
+            // 
+            this.m_activo.DataPropertyName = "m_activo";
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            this.m_activo.DefaultCellStyle = dataGridViewCellStyle5;
+            this.m_activo.HeaderText = "Activo";
+            this.m_activo.Name = "m_activo";
+            this.m_activo.ReadOnly = true;
+            this.m_activo.Width = 50;
+            // 
+            // frmUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.ClientSize = new System.Drawing.Size(963, 501);
+            this.Controls.Add(this.LProductos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmUsuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmUsuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.LProductos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataGridView LProductos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn x_producto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn i_precioUnitario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m_activo;
+    }
+}
